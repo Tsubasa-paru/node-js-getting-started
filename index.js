4bace181b8aeac8a6bb7a8cb25d902ec2ac86f09
Tsubasa-paru <tubasa_12345_67890@yahoo.co.jp>
2020-04-29 16:50:56 +0900

    add admission

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const line = require("@line/bot-sdk");
+
+const config = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET_KEY
+};
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/', (req, res) => res.json({ method: "こんにちは、getさん" })) // 追加
-  .post('/p/', (req, res) => res.json({ method: "こんにちは、postさん" })) // 追加
-  .post("/hook/", (req, res) => res.json({ test: "hook" }))// 追加
+  .get('/g/', (req, res) => res.json({ method: "こんにちは、getさん" }))
+  .post('/p/', (req, res) => res.json({ method: "こんにちは、postさん" }))
+  .post("/hook/", line.middleware(config), (req, res) => linebot(req, res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+function lineBot(req, res) {
+  res.json({ test: "hook" });
+  console.log("pass");
+}
