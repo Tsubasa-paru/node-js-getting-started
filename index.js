d360c191eb9213ee7304b2d1cae2c4943c7eb151
Tsubasa-paru <tubasa_12345_67890@yahoo.co.jp>
2020-05-04 15:21:06 +0900

    uncomment echoman

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function lineBot(req, res) {
   for (let i = 0, l = events.length; i < l; i++) {
     const ev = events[i];
     promises.push(
-      //echoman(ev)
+      echoman(ev)
       getmenu(ev)
     );
   }
