c313890c631b8ce983b420314e62c1e51534e10b
Tsubasa-paru <tubasa_12345_67890@yahoo.co.jp>
2020-10-07 10:17:30 +0900

    fix

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ async function save_file(ev) {
       stream.on('data', (chunk) => {
         fs.readFile(chunk, function (err, data) {
           if (err) throw err;
-          var name = "file.png";
+          var name = "file.jpg";
           ncmb.File.upload(name, data)
             .then(function (data) {
               // アップロード後処理
